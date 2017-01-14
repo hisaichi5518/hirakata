91e08402a3a498d24d9e54b817a70b74f54c4d57
hisaichi5518 <hisaichi5518@gmail.com>
2017-01-15 00:28:44 +0900

    サンプルをEditText+TextInputLayoutに対応

--- sample/src/main/java/com/github/hisaichi5518/hirakata/sample/MainActivity.java
+++ sample/src/main/java/com/github/hisaichi5518/hirakata/sample/MainActivity.java
@@ -1,14 +1,17 @@
 package com.github.hisaichi5518.hirakata.sample;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
 
 import com.github.hisaichi5518.hirakata.Hirakata;
 import com.github.hisaichi5518.hirakata.ValidateResult;
 import com.github.hisaichi5518.hirakata.Validator;
+import com.github.hisaichi5518.hirakata.sample.databinding.ActivityMainBinding;
 import com.github.hisaichi5518.hirakata.validationbehavior.EmailValidationBehavior;
+import com.github.hisaichi5518.hirakata.validationbehavior.NotEmptyValidationBehavior;
 
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
@@ -18,38 +21,57 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        Hirakata hirakata = new Hirakata.Builder()
-                .add((TextView) findViewById(R.id.text_email),
+        final Hirakata hirakata = new Hirakata.Builder()
+                .add(binding.textInputEmail,
+                        new Validator.Builder()
+                                .message("メールアドレスは必須です")
+                                .validationBehavior(new NotEmptyValidationBehavior())
+                                .build(),
                         new Validator.Builder()
                                 .message("メールアドレスが不正です")
                                 .validationBehavior(new EmailValidationBehavior())
                                 .build())
                 .build();
 
-
-        hirakata.validate((TextView) findViewById(R.id.text_email)).subscribe(new Observer<ValidateResult>() {
+        binding.textInputEmail.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onSubscribe(Disposable d) {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
             }
 
             @Override
-            public void onNext(ValidateResult validateResult) {
-                Log.i("onNext", validateResult.toString());
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
             }
 
             @Override
-            public void onError(Throwable e) {
-                Log.i("onError", e.toString());
-            }
+            public void afterTextChanged(Editable s) {
 
-            @Override
-            public void onComplete() {
+                hirakata.validate(binding.textInputEmail).subscribe(new Observer<ValidateResult>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                    }
 
+                    @Override
+                    public void onNext(ValidateResult validateResult) {
+                        binding.textInputEmailLayout.setErrorEnabled(false);
+                        binding.textInputEmailLayout.setError(null);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        binding.textInputEmailLayout.setErrorEnabled(true);
+                        binding.textInputEmailLayout.setError(e.getMessage());
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
             }
         });
-
     }
 }
