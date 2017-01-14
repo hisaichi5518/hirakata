4e2056057ef8fdf9e459eb12f4e7c67333156c93
hisaichi5518 <hisaichi5518@gmail.com>
2017-01-14 15:03:34 +0900

    Added Validator.Builder class

--- library/src/main/java/com/github/hisaichi5518/hirakata/Validator.java
+++ library/src/main/java/com/github/hisaichi5518/hirakata/Validator.java
@@ -2,4 +2,32 @@ package com.github.hisaichi5518.hirakata;
 
 
 public class Validator {
+    private final String message;
+    private final ValidationBehavior validationBehavior;
+
+    public Validator(String message, ValidationBehavior validationBehavior) {
+        this.message = message;
+        this.validationBehavior = validationBehavior;
+    }
+
+    public static final class Builder {
+        private String message;
+        private ValidationBehavior validationBehavior;
+
+        public Builder message(String message) {
+            this.message = message;
+
+            return this;
+        }
+
+        public Builder validationBehavior(ValidationBehavior validationBehavior) {
+            this.validationBehavior = validationBehavior;
+
+            return this;
+        }
+
+        public Validator build() {
+            return new Validator(message, validationBehavior);
+        }
+    }
 }
