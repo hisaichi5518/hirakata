ff6d9b58ca254291fd43ecfdc46f306726711bb2
hisaichi5518 <hisaichi5518@gmail.com>
2017-01-14 15:49:34 +0900

    Added Hirakata#validate(TextView)

--- library/src/main/java/com/github/hisaichi5518/hirakata/Hirakata.java
+++ library/src/main/java/com/github/hisaichi5518/hirakata/Hirakata.java
@@ -6,11 +6,24 @@ import android.widget.TextView;
 import java.util.HashMap;
 import java.util.Map;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.functions.Function;
+
 public class Hirakata {
     private final Map<TextView, Validator[]> validatorMap;
 
     private Hirakata(Map<TextView, Validator[]> validatorMap) {
         this.validatorMap = validatorMap;
+    }
+
+    public Observable<ValidateResult> validate(final TextView textView) {
+        return Observable.fromArray(validatorMap.get(textView)).flatMap(new Function<Validator, ObservableSource<ValidateResult>>() {
+            @Override
+            public ObservableSource<ValidateResult> apply(Validator validator) throws Exception {
+                return validator.validate(textView);
+            }
+        });
     }
 
     public static final class Builder {
